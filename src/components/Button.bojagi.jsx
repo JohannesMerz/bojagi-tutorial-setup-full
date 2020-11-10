e57e9a73a74373f17eb282d1c4f10b3046069811
JohannesMerz <johannes_merz@gmx.de>
2020-11-10 20:19:55 +0100

    Updated Button styles

---
+++ src/components/Button.bojagi.jsx
@@ -0,0 +1,16 @@
+// src/components/Button.bojagi.jsx
+import React from 'react';
+import { Button } from './Button.jsx';
+
+export default {
+  title: 'Button',
+};
+
+export const Normal = () =>
+  <Button>Hello</Button>;
+
+export const Outline = () =>
+  <Button variant="outline">Goodbye</Button>
+
+export const Dotted = () =>
+  <Button variant="dotted">Ob-la-di Ob-la-da</Button>
